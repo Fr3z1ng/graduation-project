3ae37173b1536e02a57144ac59db6f6c043a026b
pudge <beste1997@mail.ru>
2023-04-14 16:09:04 +0300

    - добавил фичу сбрасывание пароля и присылание ссылки на сбрасывание на почту

--- online/users/urls.py
+++ online/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import views as auth_views
+from .views import MyPasswordResetView, PasswordResetConfirmView
 from django.urls import include, path, re_path, reverse_lazy
 from . import views
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path("logout/", views.logout_view, name="logout"),
     path("password-change/", views.password_change, name="password-change"),
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
+    path('password_reset/', MyPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.password_reset_done, name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/complete/', views.password_reset_complete, name='password_reset_complete'),
 ]
--- online/users/views.py
+++ online/users/views.py
@@ -1,13 +1,19 @@
+import os
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .token import account_activation_token
 from .forms import CustomUserCreationForm, LoginForm, CustomPasswordChangeForm
 from django.core.mail import send_mail
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetView
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def register(request):
@@ -30,7 +36,7 @@ def register(request):
             send_mail(
                 mail_subject,
                 message,
-                'beste1997@mail.ru',
+                os.environ.get('EMAIL_HOST_USER'),
                 [to_email],
                 fail_silently=False,
             )
@@ -61,7 +67,6 @@ def login_view(request):
     """
     if request.method == "POST":
         form = LoginForm(request.POST)
-        next_url = request.POST.get("next")  # необходимо для перенаправление на ресурс
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(username=cd["username"], password=cd["password"])
@@ -98,3 +103,47 @@ def password_change(request):
         current_user = request.user
         form = CustomPasswordChangeForm(current_user)
     return render(request, "users/password_change.html", {"form": form})
+
+
+class MyPasswordResetView(PasswordResetView):
+    email_template_name = 'users/password_reset_email.html'  # Шаблон для отправки электронной почты с ссылкой на сброс пароля
+    html_email_template_name = 'users/password_reset_email.html'  # Шаблон для отправки HTML-версии электронной почты с ссылкой на сброс пароля
+    from_email = os.environ.get('EMAIL_HOST_USER')
+    success_url = reverse_lazy("users:password_reset_done")
+
+    def form_valid(self, form):
+        # Проверяем, существует ли email-адрес в базе данных
+        email = form.cleaned_data.get('email')
+        user = User.objects.filter(
+            email=email).first()  # Используем filter и first вместо get с аргументом default=None
+        if not user:
+            return render(self.request, 'users/reset_email_false.html')
+        return super().form_valid(form)
+
+
+class MyPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'users/password_reset_confirm.html'
+    success_url = reverse_lazy("users:password_reset_complete")
+
+
+def password_reset_done(request):
+    """
+    Представление для успешного завершения сброса пароля
+    """
+    # Ваша логика обработки успешного завершения сброса пароля, например:
+    context = {
+        'message': 'Сброс пароля успешно выполнен! Проверьте почту на наличие письма подтверждение изменения пароля',
+        # Сообщение, которое вы хотите отобразить на странице
+    }
+    return render(request, 'users/password_reset_done.html', context)
+
+
+def password_reset_complete(request):
+    """
+    Представление для успешного завершения сброса пароля
+    """
+    # Ваша логика обработки успешного завершения сброса пароля, например:
+    context = {
+        'message': 'Ваш пароль изменен',  # Сообщение, которое вы хотите отобразить на странице
+    }
+    return render(request, 'users/password_reset_complete.html', context)
